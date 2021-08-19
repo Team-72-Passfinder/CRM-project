018a769e23b7856b0ca2770b285dd70af64b61e7
NKLam Default <khoalamvn@gmail.com>
2021-08-19 17:42:54 +0700

    Test pre-push hook

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello Worlddd!');
+  res.send('Hellooo Worlddd!');
 });
 
 app.listen(3000, function () {
