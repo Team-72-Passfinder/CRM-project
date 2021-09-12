196c55b01f957c223ef8b3ba486c79af4275a64a
NKLam <khoalamvn@gmail.com>
2021-09-12 14:55:36 +0700

    Added proxy

--- client/src/api.js
+++ client/src/api.js
@@ -3,8 +3,7 @@ const axios = require('axios');
 const BASE_URL = 'http://localhost:5000';
 
 export function getEvents() {
-  let endpoint = BASE_URL + '/event';
-  return axios.get(endpoint).then((response) => response.data);
+  return axios.get('/event').then((response) => response.data);
 }
 
 export async function login(username, password) {
