c4f36d96e4994267566926b480ccbf616c6d4e88
NKLam <khoalamvn@gmail.com>
2021-09-12 11:25:39 +0700

    homepage can display events from DB

--- client/src/api.js
+++ client/src/api.js
@@ -3,7 +3,6 @@ const axios = require('axios');
 const BASE_URL = 'http://localhost:5000';
 
 export function getEvents() {
-    let endpoint = BASE_URL + '/event';
-
-    return axios.get(endpoint).then(response => response.data)
+  let endpoint = BASE_URL + '/event';
+  return axios.get(endpoint).then((response) => response.data);
 }
--- client/src/pages/home/home.js
+++ client/src/pages/home/home.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { AppBar } from '@material-ui/core';
 import { Button } from '@material-ui/core';
 import { Card } from '@material-ui/core';
@@ -11,10 +11,9 @@ import { Container } from '@material-ui/core';
 import PermanentDrawerLeft from './permanentDrawerLeft';
 import { Link } from 'react-router-dom';
 
-let data = require('./data.json')
+import { getEvents } from '../../api';
 
 const useStyles = makeStyles((theme) => ({
-  
   card: {
     height: '100%',
     display: 'flex',
@@ -26,60 +25,77 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-
-const cards = [1, 2, 3, 4, 5, 6];
+const maxCards = 6;
+const cards = Array.from(Array(maxCards).keys());
 
 function Home() {
   const classes = useStyles();
 
-  return (
-    <React.Fragment>
+  const [events, setEvents] = useState([]);
 
-      <PermanentDrawerLeft/>
-      <AppBar position="relative">
+  useEffect(() => {
+    getEvents().then((res) => {
+      setEvents(res);
+    });
+  }, []);
 
-      </AppBar>
-      <main>
-        {/* Hero unit */}
-        <div className={classes.heroContent}>
-          <Container maxWidth="sm">
-            <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
-            Welcome Back (name)
-            </Typography>
-            
+  const getDate = (date) => {
+    var jsDate = new Date(date);
+    return jsDate.toLocaleString('en-US');
+  };
+
+  if (events.length > 0) {
+    return (
+      <React.Fragment>
+        <PermanentDrawerLeft />
+        <AppBar position="relative"></AppBar>
+        <main>
+          {/* Hero unit */}
+          <div className={classes.heroContent}>
+            <Container maxWidth="sm">
+              <Typography
+                component="h1"
+                variant="h2"
+                align="center"
+                color="textPrimary"
+                gutterBottom
+              >
+                Welcome Back (name)
+              </Typography>
+            </Container>
+          </div>
+          <Container className={classes.cardGrid} maxWidth="md">
+            {/* End hero unit */}
+            <Grid container spacing={4}>
+              {cards.map((card) => (
+                <Grid item key={card} xs={12} sm={6} md={4}>
+                  <Card>
+                    <CardContent>
+                      <Typography gutterBottom variant="h5" component="h2">
+                        {events[card].name}
+                      </Typography>
+                      <Typography gutterBottom variant="h5" component="h2">
+                        {getDate(events[card].dateTime)}
+                      </Typography>
+                      <Typography>{events[card].description}</Typography>
+                    </CardContent>
+                    <CardActions>
+                      <Link to={'/event:' + events[card]._id}>
+                        <Button variant="warning" size="lg">
+                          View
+                        </Button>
+                      </Link>
+                    </CardActions>
+                  </Card>
+                </Grid>
+              ))}
+            </Grid>
           </Container>
-        </div>
-        <Container className={classes.cardGrid} maxWidth="md">
-          {/* End hero unit */}
-          <Grid container spacing={4}>
-            {cards.map((card) => (
-              <Grid item key={card} xs={12} sm={6} md={4}>
-              <Card>
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="h2">
-                {data[card].name}
-              </Typography>
-              <Typography gutterBottom variant="h5" component="h2">
-              {data[card].dateTime}
-              </Typography>
-              <Typography>
-              {data[card].description}
-              </Typography>
-            </CardContent>
-            <CardActions>
-            <Link to={"/event:"+ data[card]._id} >
-             <Button variant="warning" size="lg">
-                 View
-              </Button>
-            </Link>
-            </CardActions>
-          </Card>
-              </Grid>
-            ))}
-          </Grid>
-        </Container>
-      </main>
-    </React.Fragment>
-  );
+        </main>
+      </React.Fragment>
+    );
+  } else {
+    return <h3> Loading...</h3>;
+  }
 }
 export default Home;
