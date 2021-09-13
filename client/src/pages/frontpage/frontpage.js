122919b072b41c991134d731d65b91c78f49e0eb
NKLam <khoalamvn@gmail.com>
2021-09-13 11:13:36 +0700

    Added frontpage

---
+++ client/src/pages/frontpage/frontpage.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+
+import "./frontpage.css";
+
+function Frontpage() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <p>
+          Go to /home for homepage.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default Frontpage;
