16d830ac860018761d0303f81a3997ad20449a4a
NKLam <khoalamvn@gmail.com>
2021-09-19 21:27:49 +0700

    Finished basic layout
    Need to fix the style to be consistent. Currently I used both MaterialUI and CSS.

--- client/src/pages/frontpage/frontpage.js
+++ client/src/pages/frontpage/frontpage.js
@@ -8,20 +8,32 @@ import {
   Container,
   AppBar,
   Toolbar,
+  Grid,
+  Paper,
 } from '@material-ui/core';
 
 import './frontpage.css';
 
 const useStyles = makeStyles((theme) => ({
   root: {},
-  logo: {
-    alignSelf: 'center',
-    margin: 10,
-    width: 180,
-    height: 180,
+  heroContainer: {
+    paddingTop: 10,
+    paddingBottom: 50,
+  },
+  faqGrid: {
+    padding: 10,
+    margin: 20,
+    background: 'pink',
   },
   button: {
     margin: 10,
+  },
+  footer: {
+    color: 'white',
+    background: 'navy',
+  },
+  testColor: {
+    background: 'purple',
   },
 }));
 
@@ -48,56 +60,79 @@ function Frontpage() {
 
       <main>
         {/* Hero unit */}
-        <Box>
-          <Container maxWidth="md">
-            <img className="Logo" src={logo} alt="Logo" />
-            <Typography
-              variant="h3"
-              align="center"
-              color="text.primary"
-              gutterBottom
+        <Container className={classes.heroContainer} maxWidth="md">
+          <img className="Logo" src={logo} alt="Logo" />
+          <Typography
+            variant="h3"
+            align="center"
+            color="text.primary"
+            gutterBottom
+          >
+            Welcome to Citrus Contact!
+          </Typography>
+          <Typography variant="body1" align="center" color="text.secondary">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent
+            vestibulum nibh quis sagittis placerat. Ut nisi quam, pharetra at
+            risus a, scelerisque tristique magna. Pellentesque justo nulla,
+            venenatis eget urna ac, luctus suscipit tortor. Pellentesque vitae
+            sagittis risus. Duis non libero quis ipsum hendrerit commodo
+          </Typography>
+          <Container>
+            <Button
+              className={classes.button}
+              color="primary"
+              variant="outlined"
+              onClick={goToLogin}
             >
-              Welcome to Citrus Contact!
-            </Typography>
-            <Typography
-              variant="body1"
-              align="center"
-              color="text.secondary"
-              paragraph
-            >
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent
-              vestibulum nibh quis sagittis placerat. Ut nisi quam, pharetra at
-              risus a, scelerisque tristique magna. Pellentesque justo nulla,
-              venenatis eget urna ac, luctus suscipit tortor. Pellentesque vitae
-              sagittis risus. Duis non libero quis ipsum hendrerit commodo
-            </Typography>
-            <Container
-              sx={{ pt: 4 }}
-              direction="row"
-              spacing={2}
-              justifyContent="center"
-            >
-              <Button
-                className={classes.button}
-                color="primary"
-                variant="outlined"
-                onClick={goToLogin}
-              >
-                Go to Login page
-              </Button>
+              Login
+            </Button>
 
-              <Button
-                className={classes.button}
-                color="primary"
-                variant="contained"
-                onClick={goToRegister}
-              >
-                Register (not implemented)
-              </Button>
-            </Container>
+            <Button
+              className={classes.button}
+              color="primary"
+              variant="contained"
+              onClick={goToRegister}
+            >
+              Register
+            </Button>
           </Container>
+        </Container>
+        {/* End Hero unit */}
+        {/* FAQ Section */}
+        <Box>
+          <Grid container spacing={2}>
+            <Grid item xs>
+              <Paper elevation={3} className={classes.faqGrid}>
+                Feature number one. Lorem ipsum dolor sit amet, consectetur
+                adipiscing elit. Praesent vestibulum nibh quis sagittis
+                placerat. Ut nisi quam, pharetra at risus a, scelerisque
+                tristique magna
+              </Paper>
+            </Grid>
+            <Grid item xs>
+              <Paper elevation={3} className={classes.faqGrid}>
+                Feature number two.
+              </Paper>
+            </Grid>
+            <Grid item xs>
+              <Paper elevation={3} className={classes.faqGrid}>
+                Feature number three.
+              </Paper>
+            </Grid>
+            <Grid item xs>
+              <Paper elevation={3} className={classes.faqGrid}>
+                Feature number four.
+              </Paper>
+            </Grid>
+          </Grid>
         </Box>
+        {/* End FAQ Section */}
       </main>
+      {/* Footer */}
+      <div>
+        <p className="Footer">Made by Passfinders, 2021</p>
+      </div>
+      {/* End footer */}
     </div>
   );
 }
