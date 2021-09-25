27447196d8156040ced85bacb15edad693a9a920
NKLam <khoalamvn@gmail.com>
2021-09-25 20:01:20 +0700

    Updated to newer material-ui version
    v4 to v5

--- client/src/pages/event/editEvent.js
+++ client/src/pages/event/editEvent.js
@@ -1,15 +1,15 @@
 import React, { useState, useEffect,Component } from 'react';
-import { AppBar } from '@material-ui/core';
-import { Button } from '@material-ui/core';
-import { Card } from '@material-ui/core';
-import { CardActions } from '@material-ui/core';
-import { CardContent } from '@material-ui/core';
-import { Grid } from '@material-ui/core';
-import { Typography } from '@material-ui/core';
-import { makeStyles } from '@material-ui/styles';
-import { Container } from '@material-ui/core';
+import { AppBar } from '@mui/material';
+import { Button } from '@mui/material';
+import { Card } from '@mui/material';
+import { CardActions } from '@mui/material';
+import { CardContent } from '@mui/material';
+import { Grid } from '@mui/material';
+import { Typography } from '@mui/material';
+import { makeStyles } from '@mui/styles';
+import { Container } from '@mui/material';
 import { Link } from 'react-router-dom';
-import { Box } from '@material-ui/core';
+import { Box } from '@mui/material';
 import { EditText, EditTextarea } from 'react-edit-text';
 import 'react-edit-text/dist/index.css';
 
