8e810c83adf8d2a077985a640723b83a0b1f1332
NKLam <khoalamvn@gmail.com>
2021-09-25 20:18:06 +0700

    Switched to Benedict's NavBar

--- client/src/pages/home/permanentDrawerLeft.js
+++ client/src/pages/home/permanentDrawerLeft.js
@@ -1,103 +1,103 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Drawer from '@material-ui/core/Drawer';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Button from '@material-ui/core/Button';
-import { Link } from 'react-router-dom';
+// import React from 'react';
+// import makeStyles from '@mui/styles/makeStyles';
+// import Drawer from '@mui/material/Drawer';
+// import CssBaseline from '@mui/material/CssBaseline';
+// import Button from '@mui/material/Button';
+// import { Link } from 'react-router-dom';
 
-const drawerWidth = 180;
+// const drawerWidth = 180;
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    display: 'flex',
-  },
-  
-  appBar: {
-    width: `calc(100% - ${drawerWidth}px)`,
-    marginLeft: drawerWidth,
-  },
-  drawer: {
-    width: drawerWidth,
-    flexShrink: 0,
-  },
-  drawerPaper: {
-    width: drawerWidth,
-  },
-  // necessary for content to be below app bar
-  toolbar: theme.mixins.toolbar,
-  content: {
-    flexGrow: 1,
-    backgroundColor: theme.palette.background.default,
-    padding: theme.spacing(3),
-  },
-  button: {
-    
-    width: drawerWidth,
-    padding: theme.spacing(4),
-  },
-}));
+// const useStyles = makeStyles((theme) => ({
+//   root: {
+//     display: 'flex',
+//   },
 
-export default function PermanentDrawerLeft() {
-  const classes = useStyles();
-  const goToLogin = () => {
-    window.location.href = '/login';
-  };
-  const goToProfile = () => {
-    window.location.href = '/profile';
-  };
-  const goToChat = () => {
-    window.location.href = '/chat';
-  };
-  const goToEvent = () => {
-    window.location.href = '/events';
-  };
+//   appBar: {
+//     width: `calc(100% - ${drawerWidth}px)`,
+//     marginLeft: drawerWidth,
+//   },
+//   drawer: {
+//     width: drawerWidth,
+//     flexShrink: 0,
+//   },
+//   drawerPaper: {
+//     width: drawerWidth,
+//   },
+//   // necessary for content to be below app bar
+//   toolbar: theme.mixins.toolbar,
+//   content: {
+//     flexGrow: 1,
+//     backgroundColor: theme.palette.background.default,
+//     padding: theme.spacing(3),
+//   },
+//   button: {
 
-  return (
-    <div className={classes.root}>
-      <CssBaseline />
-      <Drawer
-        className={classes.drawer}
-        variant="permanent"
-        classes={{
-          paper: classes.drawerPaper,
-        }}
-        anchor="left"
-      >
+//     width: drawerWidth,
+//     padding: theme.spacing(4),
+//   },
+// }));
 
-<Button
-              className={classes.button}
-              color="primary"
-              variant="outlined"
-              onClick={goToEvent}
-            >
-              Events
-            </Button>
-            <Button
-              className={classes.button}
-              color="primary"
-              variant="outlined"
-              onClick={goToChat}
-            >
-              Chat
-            </Button>
-            <Button
-              className={classes.button}
-              color="primary"
-              variant="outlined"
-              onClick={goToProfile}
-            >
-              Profile
-            </Button>
-            <Button
-              className={classes.button}
-              color="primary"
-              variant="outlined"
-              onClick={goToLogin}
-            >
-              Logout
-            </Button>
-     
-      </Drawer>
-    </div>
-  );
-}
+// export default function PermanentDrawerLeft() {
+//   const classes = useStyles();
+//   const goToLogin = () => {
+//     window.location.href = '/login';
+//   };
+//   const goToProfile = () => {
+//     window.location.href = '/profile';
+//   };
+//   const goToChat = () => {
+//     window.location.href = '/chat';
+//   };
+//   const goToEvent = () => {
+//     window.location.href = '/events';
+//   };
+
+//   return (
+//     <div className={classes.root}>
+//       <CssBaseline />
+//       <Drawer
+//         className={classes.drawer}
+//         variant="permanent"
+//         classes={{
+//           paper: classes.drawerPaper,
+//         }}
+//         anchor="left"
+//       >
+
+// <Button
+//               className={classes.button}
+//               color="primary"
+//               variant="outlined"
+//               onClick={goToEvent}
+//             >
+//               Events
+//             </Button>
+//             <Button
+//               className={classes.button}
+//               color="primary"
+//               variant="outlined"
+//               onClick={goToChat}
+//             >
+//               Chat
+//             </Button>
+//             <Button
+//               className={classes.button}
+//               color="primary"
+//               variant="outlined"
+//               onClick={goToProfile}
+//             >
+//               Profile
+//             </Button>
+//             <Button
+//               className={classes.button}
+//               color="primary"
+//               variant="outlined"
+//               onClick={goToLogin}
+//             >
+//               Logout
+//             </Button>
+
+//       </Drawer>
+//     </div>
+//   );
+// }
