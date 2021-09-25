f757d2fd7e50a86267864ee00277ecbd9dace850
NKLam <khoalamvn@gmail.com>
2021-09-25 19:37:01 +0700

    Make all card same height and optimize structure

--- client/src/pages/event/event.js
+++ client/src/pages/event/event.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect,Component } from 'react';
+import React, { useState, useEffect, Component } from 'react';
 import { AppBar } from '@material-ui/core';
 import { Button } from '@material-ui/core';
 import { Card } from '@material-ui/core';
@@ -13,76 +13,63 @@ import { Box } from '@material-ui/core';
 import { EditText, EditTextarea } from 'react-edit-text';
 import 'react-edit-text/dist/index.css';
 
-
 import { getEvent } from '../../api';
 import { editEvent } from '../../api';
-const useStyles = makeStyles((theme) => ({
-
-  }));
+const useStyles = makeStyles((theme) => ({}));
 
 function Event() {
-    const classes = useStyles();
-    const [event, setEvent] = useState();
+  const classes = useStyles();
+  const [event, setEvent] = useState();
 
-    useEffect(() => {
-        let id = window.location.pathname.split('/')[2]
-        getEvent(id).then((res) => {
-          setEvent(res);
-        });
-      }, []);
-      const getDate = (date) => {
-        var jsDate = new Date(date);
-        return jsDate.toLocaleString('en-GB', {timeZone: 'UTC'});
-      };
-      const goToEdit = () => {
-        window.location.href = '/event/'+window.location.pathname.split('/')[2]+'/edit';
-      };
-    if (event!=null){
-        return ( 
-            <div className={classes.root}> 
-                {
-                    <React.Fragment>
-                        <main>
-                        <Box className={classes.formContainer}>
-                <Typography className={classes.title}>
-                    Event
-                </Typography>
+  useEffect(() => {
+    let id = window.location.pathname.split('/')[2];
+    getEvent(id).then((res) => {
+      setEvent(res);
+    });
+  }, []);
+  const getDate = (date) => {
+    var jsDate = new Date(date);
+    return jsDate.toLocaleString('en-GB', { timeZone: 'UTC' });
+  };
+  const goToEdit = () => {
+    window.location.href =
+      '/event/' + window.location.pathname.split('/')[2] + '/edit';
+  };
+  if (event != null) {
+    return (
+      <div className={classes.root}>
+        {
+          <React.Fragment>
+            <main>
+              <Box className={classes.formContainer}>
+                <Typography className={classes.title}>Event</Typography>
                 <form className={classes.form}>
-                <Box mt={5}>
-                            <Typography variant='h6'>
-                                {event.name}
-                            </Typography>
-                            <Typography variant='h6'>
-                                 {getDate(event.dateTime)}
-                            </Typography>
-                            <Typography variant='h6'>                           
-                                {event.description}
-                            </Typography>
-                            
-                    </Box>
+                  <Box mt={5}>
+                    <Typography variant="h6">{event.name}</Typography>
+                    <Typography variant="h6">
+                      {getDate(event.dateTime)}
+                    </Typography>
+                    <Typography variant="h6">{event.description}</Typography>
+                  </Box>
                 </form>
-            </Box>
-                  <div>
-                  <Button
-              className={classes.button}
-              color="primary"
-              variant="outlined"
-              onClick={goToEdit}
-            >
-              Edit
-            </Button>
-
+              </Box>
+              <div>
+                <Button
+                  className={classes.button}
+                  color="primary"
+                  variant="outlined"
+                  onClick={goToEdit}
+                >
+                  Edit
+                </Button>
+              </div>
+            </main>
+          </React.Fragment>
+        }
       </div>
-
-                
-                            
-                        </main>
-                    </React.Fragment>
-                }
-            </div>
-        );
-    } else {
-        return <h3> Loading...</h3>;
-      }
-    }
+    );
+  } else {
+    return <h3> Loading...</h3>;
+  }
+}
 export default Event;
--- client/src/pages/home/home.js
+++ client/src/pages/home/home.js
@@ -1,29 +1,30 @@
 import React, { useState, useEffect } from 'react';
-import { Button } from '@material-ui/core';
-import { Card } from '@material-ui/core';
-import { CardActions } from '@material-ui/core';
-import { CardContent } from '@material-ui/core';
-import { Grid } from '@material-ui/core';
-import { Typography } from '@material-ui/core';
+import {
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  Grid,
+  Typography,
+  Container,
+} from '@material-ui/core';
 import { makeStyles } from '@material-ui/styles';
-import { Container } from '@material-ui/core';
 import PermanentDrawerLeft from './permanentDrawerLeft';
 import { Link } from 'react-router-dom';
+
+import Navbar from '../../components/Navbar/Navbar';
 
 import { getEvents } from '../../api';
 
 const useStyles = makeStyles((theme) => ({
-  body: {
-    background: '#fff1e1',
-  },
-  heroContent:{
+  root: {
     background: '#fff1e1',
   },
   card: {
     height: '100%',
     width: '100%',
     display: 'flex',
-    
+
     flexDirection: 'column',
     background: 'blue',
   },
@@ -32,10 +33,13 @@ const useStyles = makeStyles((theme) => ({
     flexGrow: 1,
     background: 'blue',
   },
-  cardGrid:{
+  cardGrid: {
     maxwidth: 'md',
     background: '#fff1e1',
-  }
+  },
+  cardItem: {
+    height: '100%',
+  },
 }));
 
 const maxCards = 10;
@@ -54,58 +58,52 @@ function Home() {
 
   const getDate = (date) => {
     var jsDate = new Date(date);
-    return jsDate.toLocaleString('en-GB', {timeZone: 'UTC'});
+    return jsDate.toLocaleString('en-GB', { timeZone: 'UTC' });
   };
 
   if (events.length > 0) {
     return (
-      <React.Fragment>
-        <PermanentDrawerLeft />
-        <main >
-          {/* Hero unit */}
-          <div className={classes.heroContent}>
-            <Container maxWidth="sm">
-              <Typography
-                component="h1"
-                variant="h2"
-                align="center"
-                color="textPrimary"
-                gutterBottom
-              >
-                Welcome Back (name)
-              </Typography>
-            </Container>
-          
-          <Container className={classes.cardGrid} maxWidth="md">
-            {/* End hero unit */}
-            <Grid container spacing={4}>
-              {cards.map((card) => (
-                <Grid item key={card} xs={12} sm={6} md={4}>
-                  <Card>
-                    <CardContent>
-                      <Typography gutterBottom variant="h5" component="h2">
-                        {events[card].name}
-                      </Typography>
-                      <Typography gutterBottom variant="h5" component="h2">
-                        {getDate(events[card].dateTime)}
-                      </Typography>
-                      <Typography>{events[card].description}</Typography>
-                    </CardContent>
-                    <CardActions>
-                      <Link to={'/event/' + events[card]._id}>
-                        <Button variant="warning" size="lg">
-                          View
-                        </Button>
-                      </Link>
-                    </CardActions>
-                  </Card>
-                </Grid>
-              ))}
-            </Grid>
-          </Container>
-          </div>
-        </main>
-      </React.Fragment>
+      <div className={classes.root}>
+        <Navbar />
+        <Container maxWidth="sm">
+          <Typography
+            component="h1"
+            variant="h2"
+            align="center"
+            color="textPrimary"
+            gutterBottom
+          >
+            Welcome Back (name)
+          </Typography>
+        </Container>
+
+        <Container className={classes.cardGrid} maxWidth="md">
+          <Grid container spacing={4}>
+            {cards.map((card) => (
+              <Grid item key={card} xs={12} sm={6} md={4}>
+                <Card className={classes.cardItem}>
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="h2">
+                      {events[card].name}
+                    </Typography>
+                    <Typography gutterBottom variant="h5" component="h2">
+                      {getDate(events[card].dateTime)}
+                    </Typography>
+                    <Typography>{events[card].description}</Typography>
+                  </CardContent>
+                  <CardActions>
+                    <Link to={'/event/' + events[card]._id}>
+                      <Button variant="warning" size="lg">
+                        View
+                      </Button>
+                    </Link>
+                  </CardActions>
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
+        </Container>
+      </div>
     );
   } else {
     return <h3> Loading...</h3>;
