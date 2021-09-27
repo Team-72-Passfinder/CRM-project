24108988aea45e7551f77fc477b6d47a0e4afe88
haiha1199 <82726315+haiha1199@users.noreply.github.com>
2021-09-27 22:59:53 +0700

    updated user route with token

--- routes/user.js
+++ routes/user.js
@@ -2,14 +2,18 @@ const express = require('express');
 const app = express();
 
 const controller = require('../controllers/user');
+const passport = require('../config/passport');
 
-app.route('/user').post(controller.create).get(controller.findAll);
+app.use(passport.authenticate('jwt', { session: false }));
+
+app.route('/user').post(controller.create).get(controller.findAll)
+  .put(controller.update);
 
 app.route('/user/search').get(controller.search);
 
 app
   .route('/user/:id')
-  .put(controller.update)
+  //.put(controller.update)
   .delete(controller.delete)
   .get(controller.findOne);
 
