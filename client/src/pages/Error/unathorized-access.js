803fec543ce82d945767f129c0ce579a880b3665
NKLam <khoalamvn@gmail.com>
2021-10-04 11:11:55 +0700

    Make the error page look better

--- client/src/pages/Error/unathorized-access.js
+++ client/src/pages/Error/unathorized-access.js
@@ -1,13 +1,59 @@
-import { Button } from '@mui/material';
+import {
+  AppBar,
+  Button,
+  Container,
+  Link,
+  Toolbar,
+  Typography,
+} from '@mui/material';
+import { makeStyles } from '@mui/styles';
+import { Box } from '@mui/system';
 import React from 'react';
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    background: ' #fff1e1',
+  },
+  toolbar: {
+    minHeight: 48,
+    background: '#d2601a',
+  },
+}));
+
 function UnauthorizedAccessPage() {
+  const classes = useStyles();
   const goToLogin = () => (window.location.href = '/login');
 
   return (
-    <div>
-      <p>Unauthorized Acess! Please Login first!</p>
-      <Button onClick={goToLogin}>Login</Button>
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar className={classes.toolbar}>
+          <Box display="flex" flexGrow={1}>
+            <Link href="/" color="inherit">
+              <Typography variant="h6">Passfinders</Typography>
+            </Link>
+          </Box>
+        </Toolbar>
+      </AppBar>
+      <Container
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          width: '100vw',
+          height: '100vh',
+          justifyContent: 'center',
+          alignItems: 'center',
+          paddingBottom: 20,
+        }}
+        maxWidth="xs"
+      >
+        <Typography gutterBottom>
+          Unauthorized Acess! Please login first!
+        </Typography>
+        <Button variant="contained" color="primary" onClick={goToLogin}>
+          Login
+        </Button>
+      </Container>
     </div>
   );
 }
