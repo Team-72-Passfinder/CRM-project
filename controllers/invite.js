b8eea53106f774765bc9e97db0a49242a82d8fca
magicpuddingcat <danielle_rebecca_9@hotmail.com>
2021-10-06 15:05:33 +1100

    Adding structure for the invite controller

---
+++ controllers/invite.js
@@ -0,0 +1,8 @@
+// Controller to perform CRUD on event parameter
+const Event = require('../models/event');
+const controller = require('./controller-support');
+const Contact = require('../models/contact');
+
+// Create ical invite
+
+// Create invite and send (? no need to store invite as a model)
