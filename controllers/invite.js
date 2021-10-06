0cb6e3bb6d0852ccd923263a4a58e7ceca99aa5b
NKLam <khoalamvn@gmail.com>
2021-10-06 18:21:29 +0700

    Added my version of test send email
    I think I got it working, but Google blocked sussy sign-in attempt

--- controllers/invite.js
+++ controllers/invite.js
@@ -1,28 +1,66 @@
-/* Skeleton code for the emailing service using tutorial at https://www.tothenew.com/blog/sending-ical-invite-using-node-js/
-  and https://www.courier.com/blog/how-to-send-emails-with-node-js */
+/* 
+Skeleton code for the emailing service using tutorial at:
+https://www.tothenew.com/blog/sending-ical-invite-using-node-js/
+https://www.courier.com/blog/how-to-send-emails-with-node-js 
 
+Lam's used tutorial: 
+https://www.w3schools.com/nodejs/nodejs_email.asp
+https://stackoverflow.com/questions/26948516/nodemailer-invalid-login
+*/
+
+require('dotenv').config();
 const nodemailer = require('nodemailer');
 const sendgrid = require('@sendgrid/mail');
-const SENDGRID_API_KEY = "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"
 
-sendgrid.setApiKey(SENDGRID_API_KEY)
+var transporter = nodemailer.createTransport({
+  service: process.env.NODEMAILER_SERVICE,
+  auth: {
+    user: process.env.NODEMAILER_USER,
+    pass: process.env.NODEMAILER_PASS,
+  },
+});
 
-function SendInvite() {
+var mailOptions = {
+  from: 'youremail@gmail.com',
+  to: 'lamkhoan@student.unimelb.edu.au',
+  subject: 'Sending Email using Node.js',
+  text: 'That was easy!',
+};
 
-    const mailObj = {
-	  from: "citrus.contact21@gmail.com",
-      to: "drlovell@student.unimelb.edu.au",
-      subject: "Qingyi has send you an event invite",
-      text: "Dummy text for testing purposes only."
-  };
+function TestsendEmail() {
+  console.log('AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA');
+  console.log(process.env.NODEMAILER_USER);
 
-    sendgrid.send(mailObj).then((res) => {
-      console.log('Email sent\n', res)
-  })
-  .catch((err) => {
-    console.error(err)
-})
-
+  transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('Email sent: ' + info.response);
+    }
+  });
 }
 
-export default SendInvite;
+// Danielle code down here
+const SENDGRID_API_KEY =
+  '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';
+sendgrid.setApiKey(SENDGRID_API_KEY);
+
+function SendInvite() {
+  const mailObj = {
+    from: 'citrus.contact21@gmail.com',
+    to: 'drlovell@student.unimelb.edu.au',
+    subject: 'Qingyi has send you an event invite',
+    text: 'Dummy text for testing purposes only.',
+  };
+
+  sendgrid
+    .send(mailObj)
+    .then((res) => {
+      console.log('Email sent\n', res);
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+}
+
+module.exports = { SendInvite, TestsendEmail };
--- routes/invite.js
+++ routes/invite.js
@@ -5,5 +5,6 @@ const controller = require('../controllers/invite');
 
 // ? put instead of get?
 app.route('/invite').get(controller.SendInvite);
+app.route('/testinvite').get(controller.TestsendEmail);
 
 module.exports = app;
