725c064dfd2d576d0b4b991858bc0eea21cb822c
magicpuddingcat <danielle_rebecca_9@hotmail.com>
2021-10-07 18:47:39 +1100

    rejigged function to get event on FE to conform with what Benedict has done with contact; still presently not sending emails, suspected issue routing noted in the comments

--- client/src/components/EventInvite.js
+++ client/src/components/EventInvite.js
@@ -3,21 +3,27 @@ import React, { useState, useEffect } from 'react';
 import { sendEmailInvite, getEvent } from '../api';
 
 function EventInvite() {
-    const [eventId, setEventId] = useState();
+    const [event, setEvent] = useState();
 
     // ? error here; is this actually likely to capture ID as expected in a format that the API can interact with?
-    function updateEventId() {
+    useEffect(() => {
         let id = window.location.pathname.split('/')[2];
-        setEventId(id);
-    };
+        getEvent(id).then((res) => {
+          setEvent(res);
+        });
+      }, []);
 
-    return (
+    if (event != null) {
+        return (
       <div>
-          <Button onClick={sendEmailInvite(eventId)}>
+          <Button onClick={sendEmailInvite(event._id)}>
               Send invite!
           </Button>
       </div>
     );
+} else {
+    return <h3> Loading...</h3>;
+  }
 }
 
 export default EventInvite;
--- routes/invite.js
+++ routes/invite.js
@@ -4,7 +4,7 @@ const app = express();
 const controller = require('../controllers/invite');
 const eventContoller = require('../controllers/event');
 
-
+// Might also be a problem associated with this?
 app.route('/invite/:id').get(eventContoller.findOne).get(controller.SendInvite);
 
 module.exports = app;
