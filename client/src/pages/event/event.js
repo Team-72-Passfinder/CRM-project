8d3a9a3cd26a9f97a8a3707ba041796e397341ac
haiha1199 <82726315+haiha1199@users.noreply.github.com>
2021-10-14 23:47:33 +0700

    Display chips as list of participants

--- client/src/pages/event/event.js
+++ client/src/pages/event/event.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect, Component } from 'react';
 import { AppBar } from '@mui/material';
-import { Button ,Grid} from '@mui/material';
+import { Button, Grid, Stack, Chip } from '@mui/material';
 import { Typography } from '@mui/material';
 import { makeStyles } from '@mui/styles';
-import { Box,createTheme,
-  ThemeProvider, } from '@mui/material';
+import {
+  Box, createTheme,
+  ThemeProvider,
+} from '@mui/material';
 import 'react-edit-text/dist/index.css';
 
 import { getEvent } from '../../api';
@@ -37,74 +39,86 @@ function Event() {
       },
     },
   });
-  
 
-const maxCards = 10;
-let eventParticipants = Array.from(Array(maxCards).keys());
+
+  const maxCards = 10;
+  let eventParticipants = Array.from(Array(maxCards).keys());
   if (event != null) {
     eventParticipants = Array.from(Array(event.participants.length).keys());
     return (
-      <Box className={classes.root}sx={{
+      <Box className={classes.root} sx={{
         backgroundColor: 'grey'
-      }} >  
+      }} >
         {
           <React.Fragment>
             <main>
-              
-            <Navbar  />
-            <Box sx={{display: 'flex', flexDirection: 'row',  alignItems: 'center',ml: '100px', padding: '20px', backgroundColor: '#f7e0d2'
-        }} >  
 
-          <Box sx={{display: 'flex', flexDirection: 'row', width: '80vw',  alignItems: 'center',ml: '80px', backgroundColor: '#f7e0d2', justifyContent:'space-between',
-        }} >
-          <Typography variant="h5">
-                      {getDate(Date()) }
-                    </Typography>
-                    </Box>
-     <Box sx={{display: 'flex', flexDirection: 'row', width: '20vw',  alignItems: 'left',ml: '40px', backgroundColor: '#f7e0d2', justifyContent:'space-between',
-        }} >
-          <ThemeProvider theme={orangeTheme}>
-             <Button
-                  className={classes.button} color="primary" variant="contained"onClick={goToEdit}>
-                  Edit
-                </Button>
-                  <Button
-                    className={classes.button}
-                    color="primary"
-                    variant="contained">
-                    Delete
-                  </Button>
-                  </ThemeProvider>
-                 </Box>
-                 </Box>
+              <Navbar />
+              <Box sx={{
+                display: 'flex', flexDirection: 'row', alignItems: 'center', ml: '100px', padding: '20px', backgroundColor: '#f7e0d2'
+              }} >
 
-                  <Box  sx={{  align: 'center',width: '40vw', height: '100%', mt: '80px', ml: '500px', padding: '20px',display: 'flex', flexDirection: 'column', alignItems: 'left',justifyContent:'space-between',
-                 borderRadius: 4,boxShadow: 2}}>
-                   <Box sx={{display: 'flex',  alignItems: 'center',mb: '20px', backgroundColor: '#DF7861', padding: '20px',borderRadius: 4
-        }} >
-              <Typography variant="h3">{event.name}</Typography> 
-              </Box>
-              <Box sx={{display: 'flex',  alignItems: 'center',mb: '20px', backgroundColor: '#f7e0d2', padding: '20px',borderRadius: 4
-                      }} >
-                    <Typography variant="h5">
-                      Starts at: {getDate(event.startedDateTime)}
-                    </Typography>
-                    <Typography variant="h5">
-                      Ends at: {getDate(event.endedDateTime)}
-                    </Typography>
-                    </Box>
-                    <Box sx={{display: 'flex',  alignItems: 'center',mb: '20px', backgroundColor: '', padding: '20px',borderRadius: 4
-        }} >
-                    <Typography variant="h6">{event.description}</Typography>
-                    </Box>
-                    <Box sx={{display: 'flex',  alignItems: 'center',mb: '20px', padding: '20px',borderRadius: 4
-        }} >
-<Typography variant="h5"> Participants:</Typography> {event.participants.map((i) => (
-                      <Typography variant="h6">{event.participants[i]}</Typography>
-                      ))}
-      </Box>
-
+                <Box sx={{
+                  display: 'flex', flexDirection: 'row', width: '80vw', alignItems: 'center', ml: '80px', backgroundColor: '#f7e0d2', justifyContent: 'space-between',
+                }} >
+                  <Typography variant="h5">
+                    {getDate(Date())}
+                  </Typography>
                 </Box>
+                <Box sx={{
+                  display: 'flex', flexDirection: 'row', width: '20vw', alignItems: 'left', ml: '40px', backgroundColor: '#f7e0d2', justifyContent: 'space-between',
+                }} >
+                  <ThemeProvider theme={orangeTheme}>
+                    <Button
+                      className={classes.button} color="primary" variant="contained" onClick={goToEdit}>
+                      Edit
+                    </Button>
+                    <Button
+                      className={classes.button}
+                      color="primary"
+                      variant="contained">
+                      Delete
+                    </Button>
+                  </ThemeProvider>
+                </Box>
+              </Box>
+
+              <Box sx={{
+                align: 'center', width: '40vw', height: '100%', mt: '80px', ml: '500px', padding: '20px', display: 'flex', flexDirection: 'column', alignItems: 'left', justifyContent: 'space-between',
+                borderRadius: 4, boxShadow: 2
+              }}>
+                <Box sx={{
+                  display: 'flex', alignItems: 'center', mb: '20px', backgroundColor: '#DF7861', padding: '20px', borderRadius: 4
+                }} >
+                  <Typography variant="h3">{event.name}</Typography>
+                </Box>
+                <Box sx={{
+                  display: 'flex', alignItems: 'center', mb: '20px', backgroundColor: '#f7e0d2', padding: '20px', borderRadius: 4
+                }} >
+                  <Typography variant="h5">
+                    Starts at: {getDate(event.startedDateTime)}
+                  </Typography>
+                  <Typography variant="h5">
+                    Ends at: {getDate(event.endedDateTime)}
+                  </Typography>
+                </Box>
+                <Box sx={{
+                  display: 'flex', alignItems: 'center', mb: '20px', backgroundColor: '', padding: '20px', borderRadius: 4
+                }} >
+                  <Typography variant="h6">{event.description}</Typography>
+                </Box>
+                <Box sx={{
+                  display: 'flex', alignItems: 'center', mb: '20px', padding: '20px', borderRadius: 4
+                }} >
+                  <Typography variant="h5"> Participants:</Typography>
+                  <Stack sx={{ ml: '15px' }} direction="row" spacing={1}>
+                    {event.participants.map((i) => (
+                      <Chip label={i} /*href={/*Contact link goes here} clickable*/ />
+                    ))}
+                  </Stack>
+                </Box>
+
+              </Box>
             </main>
           </React.Fragment>
         }
